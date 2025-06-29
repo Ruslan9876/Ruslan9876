e8f05e93dbbc0f0133a2a3954a0fd6fd5c5e4899
KONISBAYEV RUSLAN MURATBAY ULI <125469175+Ruslan9876@users.noreply.github.com>
2025-06-29 17:46:09 +0500

    Update and rename index.html to main.py
    new test backend

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/api/hello")
+async def hello():
+    return {"message": "Привет с FastAPI backend!"}
